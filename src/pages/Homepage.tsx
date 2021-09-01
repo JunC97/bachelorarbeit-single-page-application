a24c007d7e513a96c3c6da44975a8726c03bc368
JunC97 <43088755+JunC97@users.noreply.github.com>
2021-09-01 12:35:22 +0200

    Update Homepage.tsx

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -2,8 +2,6 @@ import React, {Component} from 'react';
 import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
 
 interface HomepageProps {
-    //title?: string
-    // subtitle?: string;
      apiUrl: string;
 }
 
@@ -27,7 +25,6 @@ class Homepage extends Component<HomepageProps> {
     }
 
     render() {
-        //const { title, subtitle, children } = this.props;
         return (
             <>
                 <div className={'flex-container-row'}>
